453a87fe548480860bef226e5d7de21f8be6343d
JayBlur <98525772+SchoolAccountJeetBubna6G@users.noreply.github.com>
2025-01-28 19:05:47 +0530

    Update test_cv.py

--- test_cv.py
+++ test_cv.py
@@ -1,24 +1,20 @@
-# All the imports go here
 import cv2
 import numpy as np
 import mediapipe as mp
 from collections import deque
 
 
-# Giving different arrays to handle colour points of different colour
 bpoints = [deque(maxlen=1024)]
 gpoints = [deque(maxlen=1024)]
 rpoints = [deque(maxlen=1024)]
 ypoints = [deque(maxlen=1024)]
 
 
-# These indexes will be used to mark the points in particular arrays of specific colour
 blue_index = 0
 green_index = 0
 red_index = 0
 yellow_index = 0
 
-#The kernel to be used for dilation purpose
 kernel = np.ones((5,5),np.uint8)
 
 colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
@@ -36,8 +32,6 @@ def draw_square_with_white_border(image, center, size, color, border_thickness):
     cv2.rectangle(image, (x - half_size - border_thickness, y - half_size - border_thickness),
                   (x + half_size + border_thickness, y + half_size + border_thickness), border_color, border_thickness)
 
-
-# initialize mediapipe
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 mpDraw = mp.solutions.drawing_utils
@@ -47,17 +41,11 @@ mpDraw = mp.solutions.drawing_utils
 cap = cv2.VideoCapture(0)
 ret = True
 while ret:
-    # Read each frame from the webcam
     ret, frame = cap.read()
 
     x, y, c = frame.shape
-
-    # Flip the frame vertically
     frame = cv2.flip(frame, 1)
-    #hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    # Here is code for Canvas setup
     paintWindow = np.zeros((x, y, 3)) + 0
     paintWindow = cv2.rectangle(paintWindow, (30, 10), (110, 90), (255, 255, 255), -1)
     paintWindow = cv2.rectangle(paintWindow, (160, 10), (240, 90), (255, 0, 0), -1)
@@ -95,12 +83,9 @@ while ret:
     cv2.putText(frame, "GREEN", (300, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
     cv2.putText(frame, "RED", (440, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
     cv2.putText(frame, "YELLOW", (555, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
-    #frame = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-
-    # Get hand landmark prediction
+    #quit trying bro u wont understand its fine
     result = hands.process(framergb)
 
-    # post process the result
     if result.multi_hand_landmarks:
         landmarks = []
         for handslms in result.multi_hand_landmarks:
@@ -114,7 +99,6 @@ while ret:
                 landmarks.append([lmx, lmy])
 
 
-            # Drawing landmarks on frames
             mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
         fore_finger = (landmarks[8][0],landmarks[8][1])
         center = fore_finger
@@ -161,7 +145,6 @@ while ret:
                 rpoints[red_index].appendleft(center)
             elif colorIndex == 3:
                 ypoints[yellow_index].appendleft(center)
-    # Append the next deques when nothing is detected to avois messing up
     else:
         bpoints.append(deque(maxlen=512))
         blue_index += 1
@@ -172,7 +155,6 @@ while ret:
         ypoints.append(deque(maxlen=512))
         yellow_index += 1
 
-    # Draw lines of all the colors on the canvas and frame
     points = [bpoints, gpoints, rpoints, ypoints]
     # for j in range(len(points[0])):
     #         for k in range(1, len(points[0][j])):
@@ -193,6 +175,5 @@ while ret:
     if cv2.waitKey(1) == ord('q'):
         break
 
-# release the webcam and destroy all active windows
 cap.release()
 cv2.destroyAllWindows()
